42ade80ef191e2e433b1eb539168f07eca87ad98
poairtronic <posuppportairtronic@gmail.com>
2026-07-02 15:56:42 +0530

    sec: add helmet headers and global exception filter for sanitization

--- ecr-system/dist/main.js
+++ ecr-system/dist/main.js
@@ -5,10 +5,14 @@ const common_1 = require("@nestjs/common");
 const config_1 = require("@nestjs/config");
 const app_module_1 = require("./app.module");
 const cookieParser = require("cookie-parser");
+const helmet_1 = require("helmet");
+const all_exceptions_filter_1 = require("./common/filters/all-exceptions.filter");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     const config = app.get(config_1.ConfigService);
     app.use(cookieParser());
+    app.use((0, helmet_1.default)());
+    app.useGlobalFilters(new all_exceptions_filter_1.AllExceptionsFilter());
     const frontendUrl = config.get('FRONTEND_URL') ?? 'http://localhost:5173';
     app.enableCors({
         origin: frontendUrl,
--- ecr-system/src/main.ts
+++ ecr-system/src/main.ts
@@ -3,6 +3,8 @@ import { ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import helmet from 'helmet';
+import { AllExceptionsFilter } from './common/filters/all-exceptions.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,12 @@ async function bootstrap() {
 
   // Enable cookie parsing for HttpOnly JWT cookie auth
   app.use(cookieParser());
+
+  // Security Headers
+  app.use(helmet());
+
+  // Global Error Handler to sanitize stack traces
+  app.useGlobalFilters(new AllExceptionsFilter());
 
   const frontendUrl = config.get('FRONTEND_URL') ?? 'http://localhost:5173';
   app.enableCors({
